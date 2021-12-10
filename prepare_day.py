638a49abc5af8400e20afab8a792ca16cdb38bb5
Ryan Stark <rstark13@umd.edu>
2021-12-10 12:07:52 -0500

    Added script to prepare day

---
+++ prepare_day.py
@@ -0,0 +1,19 @@
+from os import mkdir, environ
+from sys import argv
+from pathlib import Path
+from dotenv import load_dotenv
+from requests import get
+
+DAY = argv[1]
+
+load_dotenv()
+cookies = {
+    'session': environ.get('AOC_SESSION'),
+}
+response = get(
+    'https://adventofcode.com/2021/day/{}/input'.format(DAY), cookies=cookies)
+
+mkdir('{0}/day{1}'.format(str(Path.cwd()), DAY))
+
+with open('{0}/day{1}/input.txt'.format(str(Path.cwd()), DAY), 'w') as f:
+    f.write(response.text)
